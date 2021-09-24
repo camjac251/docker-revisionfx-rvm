f4acc81b3a801e46a91c1c701f04299ce432cf07
camjac251 <camjac251@users.noreply.github.com>
2021-09-24 13:45:57 -0500

    Add diagnostic file creation

--- start.sh
+++ start.sh
@@ -8,6 +8,7 @@ hostname -i
 cp /opt/rlm/licenses/foundry_float.lic /opt/foundry_float.lic
 
 # Output info
+/usr/local/foundry/LicensingTools7.3/FoundryLicenseUtility -d
 /usr/local/foundry/LicensingTools7.3/FoundryLicenseUtility -i
 /usr/local/foundry/LicensingTools7.3/FoundryLicenseUtility -n
 /usr/local/foundry/LicensingTools7.3/FoundryLicenseUtility -v
