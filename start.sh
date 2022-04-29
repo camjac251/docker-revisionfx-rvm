95ba4fe11c797012af9e6cba2c0f0f957682c462
camjac251 <camjac251@users.noreply.github.com>
2022-04-29 09:57:06 -0500

    simplify

--- start.sh
+++ start.sh
@@ -4,15 +4,17 @@ hostname
 echo "IP ADDRESS:"
 hostname -i
 
+export RLM_LICENSE=/opt/rlm/licenses
+
 # Store the licese file inside the container for later reuse.
-cp /opt/rlm/licenses/foundry_float.lic /opt/foundry_float.lic
+#cp /opt/rlm/licenses/foundry_float.lic /opt/foundry_float.lic
 
 # Output info
-echo "Y" | /usr/local/foundry/LicensingTools7.3/FoundryLicenseUtility -d
-#echo "Y" | /usr/local/foundry/LicensingTools7.3/FoundryLicenseUtility -i
-#echo "Y" | /usr/local/foundry/LicensingTools7.3/FoundryLicenseUtility -n
-#echo "Y" | /usr/local/foundry/LicensingTools7.3/FoundryLicenseUtility -v
+echo "N" | /usr/local/foundry/LicensingTools7.3/FoundryLicenseUtility -d
+#echo "N" | /usr/local/foundry/LicensingTools7.3/FoundryLicenseUtility -i
+#echo "N" | /usr/local/foundry/LicensingTools7.3/FoundryLicenseUtility -n
+#echo "N" | /usr/local/foundry/LicensingTools7.3/FoundryLicenseUtility -v
 
 # Run the license server directly with the copied license file.
 
-echo "Y" | /usr/local/foundry/LicensingTools7.3/bin/RLM/rlm.foundry -iai -c /opt/foundry_float.lic
+echo "Y" | /usr/local/foundry/LicensingTools7.3/bin/RLM/rlm.foundry -iai
